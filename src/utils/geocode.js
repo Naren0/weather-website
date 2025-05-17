988ab686a6cb3174a264c6cceadb9925818c321d
Naren <gorantla04@gmail.com>
2025-05-17 23:49:55 +0530

    udpated the app.js file

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('postman-request');
 const geocode = (address, callback) => {
-    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibmFyZW5kcmE2NDI3IiwiYSI6ImNtYW8zanB1bDAyc2UyanNhbmNkdW9iOWoifQ.TRBjI9jb6Enq1w5QM-hcdA&limit=1&language=en';
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=test&limit=1&language=en';
 
 
     
